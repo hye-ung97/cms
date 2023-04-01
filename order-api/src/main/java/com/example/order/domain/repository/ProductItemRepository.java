3968a3bc7638965e9e89b3b13aad1bd55a2f793e
hye-young <hysung714@gmail.com>
2023-04-01 11:22:53 +0900

    feat : [009-Product_ADD] Product Add, ProductItem Add
    추가로 Exception 에 대한 Response 의 Visuality 향상

---
+++ order-api/src/main/java/com/example/order/domain/repository/ProductItemRepository.java
@@ -0,0 +1,9 @@
+package com.example.order.domain.repository;
+
+import com.example.order.domain.model.ProductItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductItemRepository extends JpaRepository<ProductItem, Long> {
+}
